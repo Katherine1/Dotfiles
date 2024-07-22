9e69aa4b25dedb6fd2a0bb076b22775c2f93b409
Emily Marriott <emilylynmarriott@proton.me>
2024-07-22 12:29:15 -0500

    Make undodir OS dependent for work computer

--- private_dot_config/nvim/lua/essentials/settings.lua
+++ private_dot_config/nvim/lua/essentials/settings.lua
@@ -1,3 +1,5 @@
+local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+
 vim.opt.tabstop=4
 vim.opt.softtabstop=4
 vim.opt.shiftwidth=4
@@ -13,7 +15,11 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("XDG_CACHE_HOME") .. "/nvim/undodir"
+if is_windows then
+    vim.opt.undodir = os.getenv("LOCALAPPDATA") .. "/nvim-data/undodir"
+else
+    vim.opt.undodir = os.getenv("XDG_CACHE_HOME") .. "/nvim/undodir"
+end
 vim.opt.undofile = true
 
 vim.opt.hlsearch = true
