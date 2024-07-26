106dbd045e1298f553361b63d85da0c86819d59b
Emily Marriott <emilylynmarriott@proton.me>
2024-07-26 06:42:32 -0500

    Switch to the Nerd Font version of Maple Mono in Neovim.

--- private_dot_config/nvim/lua/essentials/settings.lua
+++ private_dot_config/nvim/lua/essentials/settings.lua
@@ -2,7 +2,7 @@ vim.opt.tabstop=4
 vim.opt.softtabstop=4
 vim.opt.shiftwidth=4
 vim.opt.expandtab=true
-vim.opt.guifont='Maple Mono'
+vim.opt.guifont='Maple Mono NF'
 
 vim.opt.nu=true
 vim.opt.relativenumber = true
