65ca0b28893f4b30896a526d971ddf45b9c7fe6b
Emily Marriott <emilylynmarriott@proton.me>
2024-11-01 11:07:38 -0500

    Explicitly lazy load most of the things.

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -26,19 +26,19 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{'nvim-telescope/telescope.nvim', branch= '0.1.x', dependencies = {'nvim-lua/plenary.nvim'}},
+	{'nvim-telescope/telescope.nvim', branch= '0.1.x', dependencies = {'nvim-lua/plenary.nvim'}, lazy = true},
     {'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
-    {'nvim-treesitter/nvim-treesitter', run = {':TSUpdate'}},
-    {'mbbill/undotree'},
+    {'nvim-treesitter/nvim-treesitter', run = {':TSUpdate'}, lazy = true},
+    {'mbbill/undotree', lazy = true},
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', lazy = true, config = false},
-	{'neovim/nvim-lspconfig'},
+	{'neovim/nvim-lspconfig', lazy = true},
     {'saghen/blink.cmp', version = 'v0.*', dependencies = {'rafamadriz/friendly-snippets'}, lazy = false},
-    {'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim', dependencies = {'williamboman/mason.nvim', 'neovim/nvim-lspconfig'}},
+    {'williamboman/mason.nvim', lazy = true},
+	{'williamboman/mason-lspconfig.nvim', dependencies = {'williamboman/mason.nvim', 'neovim/nvim-lspconfig'}, lazy = true},
     {'christoomey/vim-tmux-navigator', lazy = false},
-    {'nvim-tree/nvim-web-devicons'},
+    {'nvim-tree/nvim-web-devicons', lazy = true},
     {'nvim-lualine/lualine.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}},
-    {'lewis6991/gitsigns.nvim'},
+    {'lewis6991/gitsigns.nvim', lazy = true},
     {'romgrk/barbar.nvim', dependencies = {'lewis6991/gitsigns.nvim', 'nvim-tree/nvim-web-devicons'}, version = '^1.0.0'}
 --    {'m4xshen/hardtime.nvim', dependencies = {'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim'}, opts = {} }
 --    {'vhyrro/luarocks.nvim', priority=1000, config=true},
