922f0ed051e818637fb2360e899ba2063bd11e71
Emily Marriott <emilylynmarriott@proton.me>
2024-12-26 21:08:38 -0600

    Remove lspzero as it is no longer developed

--- private_dot_config/nvim/after/plugin/lsp.lua
+++ private_dot_config/nvim/after/plugin/lsp.lua
@@ -1,20 +1,47 @@
-local lsp_zero = require('lsp-zero')
-lsp_zero.extend_lspconfig()
+vim.opt.signcolumn = 'yes'
 
-lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+    'force',
+    lspconfig_defaults.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
+)
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local opts = {buffer = event.buf}
+
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    end,
+})
+
+--local lsp_zero = require('lsp-zero')
+--lsp_zero.extend_lspconfig()
+
+--lsp_zero.on_attach(function(client, bufnr)
+--  local opts = {buffer = bufnr, remap = false}
+
+--  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+--  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+--  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+--  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+--  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+--  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+--  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+--  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+--  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+--end)
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
@@ -22,7 +49,10 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {'powershell_es', 'rust_analyzer', 'clangd', 'csharp_ls', 'cmake', 'gopls', 'jdtls', 'sqlls', 'pylsp'},
   handlers = {
-    lsp_zero.default_setup,
+--    lsp_zero.default_setup,
+    function(server_name)
+        require('lspconfig')[server_name].setup({})
+    end,
     lua_ls = function()
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
@@ -41,7 +71,7 @@ cmp.setup({
         {name = 'luasnip', keyword_length = 2},
         {name = 'buffer', keyword_length = 3},
     },
-    formatting = lsp_zero.cmp_format(),
+--    formatting = lsp_zero.cmp_format(),
     mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
