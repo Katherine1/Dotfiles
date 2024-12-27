d4c0b26e60d0340afb35f3418f18f2600584b6b3
Emily Marriott <emilylynmarriott@proton.me>
2024-12-26 20:40:59 -0600

    Switch back from blink.cmp to nvim-cmp

--- private_dot_config/nvim/after/plugin/lsp.lua
+++ private_dot_config/nvim/after/plugin/lsp.lua
@@ -30,30 +30,30 @@ require('mason-lspconfig').setup({
   }
 })
 
---local cmp = require('cmp')
---local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
---cmp.setup({
---    sources = {
---        {name = 'path'},
---        {name = 'nvim_lsp'},
---        {name = 'nvim_lua'},
---        {name = 'luasnip', keyword_length = 2},
---        {name = 'buffer', keyword_length = 3},
---    },
---    formatting = lsp_zero.cmp_format(),
---    mapping = cmp.mapping.preset.insert({
---        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
---        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+cmp.setup({
+    sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lua'},
+        {name = 'luasnip', keyword_length = 2},
+        {name = 'buffer', keyword_length = 3},
+    },
+    formatting = lsp_zero.cmp_format(),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         --['<C-y>'] = cmp.mapping.confirm({ select = true }),
---        ['<Tab>'] = cmp.mapping.confirm({select = true}),
---        ['<C-Space>'] = cmp.mapping.complete(),
---    }),
---})
-
-require('blink.cmp').setup({
-    nerd_font_variant = 'mono',
-    keymap = {
-        preset = 'default'
-    }
+        ['<Tab>'] = cmp.mapping.confirm({select = true}),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
 })
+
+--require('blink.cmp').setup({
+--    nerd_font_variant = 'mono',
+--    keymap = {
+--        preset = 'default'
+--    }
+--})
