ca3f1055601fb2305dc9f748c44d38a3dd9b079c
Emily Marriott <emilylynmarriott@proton.me>
2025-01-06 15:49:08 -0600

    Fix up lspconfig whitespace

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ return {
     opts = true,
     config = function ()
         local lspconfig_defaults = require('lspconfig').util.default_config
-            lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
             'force',
             lspconfig_defaults.capabilities,
             require('blink.cmp').get_lsp_capabilities()
