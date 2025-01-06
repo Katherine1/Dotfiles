6b9e1d6ac9bcecf3f51b72d99fdbd35cb9b7cf1b
Emily Marriott <emilylynmarriott@proton.me>
2025-01-06 15:00:17 -0600

    Clean up neocmake config

--- private_dot_config/nvim/lua/lsp/neocmake.lua
+++ private_dot_config/nvim/lua/lsp/neocmake.lua
@@ -9,23 +9,4 @@ return {
     end,
     single_file_support = true,
   },
-  docs = {
-    description = [[
-https://github.com/Decodetalkers/neocmakelsp
-
-CMake LSP Implementation
-
-Neovim does not currently include built-in snippets. `neocmakelsp` only provides completions when snippet support is enabled. To enable completion, install a snippet plugin and add the following override to your language client capabilities during setup.
-
-```lua
---Enable (broadcasting) snippet capability for completion
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-require'lspconfig'.neocmake.setup {
-  capabilities = capabilities,
-}
-```
-]],
-  },
 }
