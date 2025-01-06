1abc673dbfa63ebfce17e4ef8401737394aacfc0
Emily Marriott <emilylynmarriott@proton.me>
2025-01-06 08:44:15 -0600

    Switch Harpoon back to default window for now

--- private_dot_config/nvim/lua/plugins/harpoon.lua
+++ private_dot_config/nvim/lua/plugins/harpoon.lua
@@ -26,9 +26,9 @@ return {
             }):find()
         end
 
-        vim.keymap.set('n', '<C-e>', function() toggle_telescope(harpoon:list()) end, { desc = 'Open harpoon window' })
-        vim.keymap.set("n", "<C-d>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        
+--        vim.keymap.set('n', '<C-e>', function() toggle_telescope(harpoon:list()) end, { desc = 'Open harpoon window' })
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
         vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
