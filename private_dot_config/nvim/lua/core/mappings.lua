00e5781fd2d2a5b3458e9029fd056bd3e469646c
Emily Marriott <emilylynmarriott@proton.me>
2025-01-09 09:48:23 -0600

    Add light/dark mode toggles

--- private_dot_config/nvim/lua/core/mappings.lua
+++ private_dot_config/nvim/lua/core/mappings.lua
@@ -6,3 +6,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("v", "p", '"_dP')
 
+vim.keymap.set("n", "<leader>gtl", function () vim.o.background = "light" end)
+vim.keymap.set("n", "<leader>gtd", function () vim.o.background = "dark" end)
+
