40fe7f7dae1a08ebba8e5fc23f8d558f0bab1d00
Emily Marriott <emilylynmarriott@proton.me>
2025-01-18 09:35:09 -0600

    Add clangd config

---
+++ private_dot_config/nvim/lua/lsp/clangd.lua
@@ -0,0 +1,24 @@
+return {
+  default_config = {
+    cmd = { 'clangd' },
+    filetypes = { 'c', 'cpp', 'cxx', 'objc', 'objcpp', 'cuda', 'proto', 'h', 'hpp' },
+    root_markers = {
+        '.clangd',
+        '.clang-tidy',
+        '.clang-format',
+        'CMakeLists.txt',
+        'compile_commands.json',
+        'compile_flags.txt',
+        'configure.ac' -- AutoTools
+    },
+    single_file_support = true,
+    capabilities = {
+      textDocument = {
+        completion = {
+          editsNearCursor = true,
+        },
+      },
+      offsetEncoding = { 'utf-8', 'utf-16' },
+    },
+  }
+}
