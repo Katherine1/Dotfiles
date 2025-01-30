ecd22dead2703a9a533d99407303ac2e60a549ab
Emily Marriott <emilylynmarriott@proton.me>
2025-01-30 09:31:28 -0600

    Make the trailing space autocmd not active on Windows.

--- private_dot_config/nvim/lua/core/autocmds.lua
+++ private_dot_config/nvim/lua/core/autocmds.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = {"*"},
-    command = [[%s/\s\+$//e]],
-})
+if not vim.fn.has("win32") == 1 and not vim.fn.has("win64") == 1 then
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+        pattern = {"*"},
+        command = [[%s/\s\+$//e]],
+    })
+end
