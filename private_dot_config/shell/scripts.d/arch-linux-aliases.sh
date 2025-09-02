d08c9287dad2a7e89b1b27a48a656c09b20994a2
Emily Marriott <emilylynmarriott@proton.me>
2025-09-02 09:22:50 -0500

    Add function autoloading to zsh
    Mimics fish's function system

--- private_dot_config/shell/scripts.d/arch-linux-aliases.sh
+++ private_dot_config/shell/scripts.d/arch-linux-aliases.sh
@@ -3,7 +3,11 @@
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     if [ "$ID" = "arch" ]; then
-        alias yay="paru"
-        alias yeet="paru -Rcs"
+        if command -v paru >/dev/null 2>&1; then 
+            alias yay="paru"
+            alias yeet="paru -Rcs"
+        elif command -v yay >/dev/null 2>&1; then
+            alias yeet="yay -Rcs"
+        fi
     fi
 fi
